02073f630092ab8c6b6d15298c0cef82615a02fc
Megha Bhakta <meghabhakta3@gmail.com>
2021-01-20 19:50:18 +0530

    Angular Template-driven Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,13 @@ import { FlexLayoutModule} from '@angular/flex-layout';
 import { MenuComponent} from './Component/menu/menu.component';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatIconModule} from '@angular/material/icon';
+import { MatDialogModule} from '@angular/material/dialog';
 import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
+import { FormsModule} from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule} from '@angular/material/checkbox';
 import { DishdetailComponent } from './Component/dishdetail/dishdetail.component';
 import { DishService} from 'src/app/service/dish.service';
 import { HeaderComponent } from './Component/header/header.component';
@@ -22,6 +27,7 @@ import { ContactComponent } from './Component/contact/contact.component';
 
 import { RouterModule} from '@angular/router';
 import { routes} from '../app/route';
+import { LoginComponent } from './Component/login/login.component';
 
 
 @NgModule({
@@ -34,6 +40,7 @@ import { routes} from '../app/route';
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    LoginComponent,
 
   ],
   imports: [
@@ -46,10 +53,18 @@ import { routes} from '../app/route';
     MatGridListModule,
     MatCardModule,
     MatIconModule,
+    MatDialogModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [DishService],
+  entryComponents:[
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
